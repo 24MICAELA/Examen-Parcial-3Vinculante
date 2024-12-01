b5d82685ccfce334699ec19f35c15b863ec93fc6
24MICAELA <mhuermor@myuax.com>
2024-12-01 03:18:22 +0100

    version 1

--- Actor/Actor.cpp
+++ Actor/Actor.cpp
@@ -3,3 +3,36 @@
 //
 
 #include "Actor.h"
+#include <iostream>
+#include <iomanip> // Para formatear la salida
+
+// Constructor de la clase Actor. Inicializa los atributos id y descripcion.
+Actor::Actor(const std::string& id, const std::string& descripcion)
+    : id(id), descripcion(descripcion) {}
+
+// Devuelve el id del Actor.
+std::string Actor::getId() const {
+    return id;
+}
+
+// Devuelve la descripción del Actor.
+std::string Actor::getDesc() const {
+    return descripcion;
+}
+
+// Agrega una tarea a la lista de tareas del actor y devuelve el tamaño de la lista.
+int Actor::addTarea(const Tarea& t) {
+    listaTareas.push_back(t);  // Agrega la tarea al final de la lista.
+    return listaTareas.size(); // Devuelve el nuevo tamaño de la lista de tareas.
+}
+
+// Calcula la duración total de todas las tareas del Actor sumando sus duraciones.
+int Actor::getDuracionTotal() const {
+    int total = 0;
+    for (const auto& tarea : listaTareas) {  // Recorre todas las tareas de la lista.
+        total += tarea.getDuracion(); // Suma la duración de cada tarea.
+    }
+    return total; // Retorna la duración total.
+}
+
+// Devuelve una cadena que representa al Actor y sus tareas, con la descr
--- Actor/Actor.h
+++ Actor/Actor.h
@@ -5,12 +5,36 @@
 #ifndef ACTOR_H
 #define ACTOR_H
 
+#include <vector>
+#include "../Tarea/Tarea.h"
+#include <string>
 
-
+// Clase Actor que representa a un actor con un id, una descripción y una lista de tareas.
 class Actor {
+private:
+    std::string id; // Identificador del actor.
+    std::string descripcion; // Descripción del actor.
+    std::vector<Tarea> listaTareas; // Lista de tareas asignadas al actor.
 
+public:
+    // Constructor de la clase Actor. Inicializa el id y la descripción del actor.
+    Actor(const std::string& id, const std::string& descripcion);
+
+    // Devuelve el id del actor.
+    std::string getId() const;
+
+    // Devuelve la descripción del actor.
+    std::string getDesc() const;
+
+    // Agrega una tarea a la lista de tareas del actor y devuelve el tamaño actualizado de la lista.
+    int addTarea(const Tarea& t);
+
+    // Calcula la duración total de todas las tareas asignadas al actor.
+    int getDuracionTotal() const;
+
+    // Devuelve una cadena de texto con la representación del actor y sus tareas.
+    std::string toString() const;
 };
 
-
-
 #endif //ACTOR_H
+
