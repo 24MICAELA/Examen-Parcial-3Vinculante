34df4f74a6e0f569f84b2aa2b313e1b46daed9c5
24MICAELA <mhuermor@myuax.com>
2024-12-01 03:22:37 +0100

    version 2

--- Crud/Crud.cpp
+++ Crud/Crud.cpp
@@ -2,4 +2,20 @@
 // Created by Micae on 01/12/2024.
 //
 
-#include "Crud.h"
+#ifndef CRUD_H
+#define CRUD_H
+
+// Función para crear un nuevo registro o elemento.
+void crear();
+
+// Función para leer o mostrar un registro o elemento existente.
+void leer();
+
+// Función para actualizar un registro o elemento existente.
+void actualizar();
+
+// Función para borrar un registro o elemento existente.
+void borrar();
+
+#endif //CRUD_H
+
